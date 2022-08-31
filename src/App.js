dd0ea9451ad5e62788df592390c146d70107b6a4
orhunaltay <110339989+orhunaltay@users.noreply.github.com>
2022-08-31 23:05:33 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import {Footer,Blog,Possibility,Header,WhatChainStorage,Features} from './containers';
+import { CTA,Brand,Navbar } from './components';
+
+
+import './App.css';
+
+
+
+
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatChainStorage />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+      
+
+
+    </div>
+  )
+}
+
+export default App
